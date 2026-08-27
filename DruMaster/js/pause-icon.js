3d81b371cb9bf8e167d63d799acdd699ab325974
ld1kanae <aoka45utau@gmail.com>
2026-08-28 02:36:30 +0900

    Load restored pause hover glow

--- DruMaster/js/pause-icon.js
+++ DruMaster/js/pause-icon.js
@@ -44,6 +44,6 @@
 
   /* Load the finalized PC-only hover enhancer after the transport SVG setup. */
   const s=document.createElement("script");
-  s.src="js/glass-hover-final.js?v=20260828-pauseinline1";
+  s.src="js/glass-hover-final.js?v=20260828-pauseglow1";
   document.body.appendChild(s);
 })();
