4fa471f248249a11f2ec3bbac8c4c35b4df1921c
ld1kanae <aoka45utau@gmail.com>
2026-08-27 21:53:47 +0900

    Auto replay glass hover animation preview

---
+++ DruMaster/js/glass-hover-animation-preview.js
@@ -0,0 +1,45 @@
+"use strict";
+(()=>{
+  const demos=[...document.querySelectorAll(".hover-demo")];
+  const replay=document.querySelector("#replayNow");
+  if(!demos.length)return;
+
+  const ACTIVE_MS=1150;
+  const INTERVAL_MS=3000;
+  let timer=0;
+  const removers=new WeakMap();
+
+  function pulse(el){
+    const old=removers.get(el);
+    if(old)clearTimeout(old);
+    el.classList.remove("demo-active");
+    void el.offsetWidth;
+    el.classList.add("demo-active");
+    removers.set(el,setTimeout(()=>el.classList.remove("demo-active"),ACTIVE_MS));
+  }
+
+  function pulseAll(){
+    demos.forEach(pulse);
+  }
+
+  function restartTimer(){
+    clearInterval(timer);
+    timer=setInterval(pulseAll,INTERVAL_MS);
+  }
+
+  demos.forEach(el=>{
+    el.addEventListener("mouseenter",()=>pulse(el));
+    el.addEventListener("focus",()=>pulse(el));
+    el.querySelector("button")?.addEventListener("click",e=>e.preventDefault());
+  });
+
+  replay?.addEventListener("click",()=>{
+    pulseAll();
+    restartTimer();
+  });
+
+  setTimeout(()=>{
+    pulseAll();
+    restartTimer();
+  },500);
+})();
