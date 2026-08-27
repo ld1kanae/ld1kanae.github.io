0cac7bb2e851e40e7388c83d98d32c64a7da2e05
ld1kanae <aoka45utau@gmail.com>
2026-08-28 03:32:28 +0900

    Load mobile glass tap enhancer

--- DruMaster/js/pause-icon.js
+++ DruMaster/js/pause-icon.js
@@ -41,6 +41,6 @@
 
   /* Load the finalized PC-only hover enhancer after the transport SVG setup. */
   const s=document.createElement("script");
-  s.src="js/glass-hover-final.js?v=20260828-pausestable1";
+  s.src="js/glass-hover-final.js?v=20260828-mobiletap1";
   document.body.appendChild(s);
 })();
