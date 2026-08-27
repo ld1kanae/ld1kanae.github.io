13c86f392978ba213f2b08494c022d48f76c561b
ld1kanae <aoka45utau@gmail.com>
2026-08-27 15:55:04 +0900

    Replace broken loop SVG with repeat icon

--- DruMaster/js/score-playback-loop-glyph.js
+++ DruMaster/js/score-playback-loop-glyph.js
@@ -4,7 +4,7 @@
   const SVG={
     previous:'<svg class="transport-icon" viewBox="0 0 24 24" aria-hidden="true"><path d="M6 5v14"/><path class="fill" d="M18 5.5 9 12l9 6.5z"/></svg>',
     next:'<svg class="transport-icon" viewBox="0 0 24 24" aria-hidden="true"><path d="M18 5v14"/><path class="fill" d="m6 5.5 9 6.5-9 6.5z"/></svg>',
-    loop:'<svg class="transport-icon" viewBox="0 0 24 24" aria-hidden="true"><path d="M7.5 7h8.2c2.2 0 4 1.8 4 4v.4"/><path d="m16.7 4 3 3-3 3"/><path d="M16.5 17H8.3c-2.2 0-4-1.8-4-4v-.4"/><path d="m7.3 20-3-3 3-3"/></svg>',
+    loop:'<svg class="transport-icon" viewBox="0 0 24 24" aria-hidden="true"><path d="M17 2.5 21 6.5 17 10.5"/><path d="M3 11V9.5A3.5 3.5 0 0 1 6.5 6H21"/><path d="M7 21.5 3 17.5 7 13.5"/><path d="M21 13v1.5a3.5 3.5 0 0 1-3.5 3.5H3"/></svg>',
     pause:'<svg class="transport-icon" viewBox="0 0 24 24" aria-hidden="true"><rect class="fill" x="7" y="5" width="3.5" height="14" rx="1"/><rect class="fill" x="13.5" y="5" width="3.5" height="14" rx="1"/></svg>',
     play:'<svg class="transport-icon" viewBox="0 0 24 24" aria-hidden="true"><path class="fill" d="M8 5.5 18 12 8 18.5z"/></svg>'
   };
