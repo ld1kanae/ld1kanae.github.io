16a40e6af911fd40cd3ad56b5582c5dea0b531fa
ld1kanae <aoka45utau@gmail.com>
2026-08-27 22:52:07 +0900

    Load approved original hover animation build

--- DruMaster/js/pause-icon.js
+++ DruMaster/js/pause-icon.js
@@ -44,6 +44,6 @@
 
   /* Load the finalized PC-only hover enhancer after the transport SVG setup. */
   const s=document.createElement("script");
-  s.src="js/glass-hover-final.js?v=20260827-approved1";
+  s.src="js/glass-hover-final.js?v=20260827-originalanim1";
   document.body.appendChild(s);
 })();
