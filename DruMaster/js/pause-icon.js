3870c41ba55400cf03d043497da56e84279a0b24
ld1kanae <aoka45utau@gmail.com>
2026-08-28 09:29:01 +0900

    Remove pause icon shadow source

--- DruMaster/js/pause-icon.js
+++ DruMaster/js/pause-icon.js
@@ -5,35 +5,11 @@
   const panel=document.querySelector("#pausePanel");
   if(!button||!panel)return;
 
-  const SVG_NS="http://www.w3.org/2000/svg";
-
-  function whiteShape(tag,attrs){
-    const node=document.createElementNS(SVG_NS,tag);
-    for(const [name,value] of Object.entries(attrs))node.setAttribute(name,value);
-    node.setAttribute("fill","#fff");
-    node.style.setProperty("fill","#fff","important");
-    return node;
-  }
-
   function makeIcon(){
-    const svg=document.createElementNS(SVG_NS,"svg");
-    svg.setAttribute("class","pause-white-icon");
-    svg.setAttribute("viewBox","0 0 18 18");
-    svg.setAttribute("aria-hidden","true");
-    svg.setAttribute("focusable","false");
-    svg.setAttribute("fill","#fff");
-    svg.style.setProperty("fill","#fff","important");
-
-    const pause=document.createElementNS(SVG_NS,"g");
-    pause.setAttribute("class","pause-shape");
-    pause.append(
-      whiteShape("rect",{x:"2",y:"1",width:"4",height:"16",rx:"2"}),
-      whiteShape("rect",{x:"12",y:"1",width:"4",height:"16",rx:"2"})
-    );
-
-    const play=whiteShape("path",{class:"play-shape",d:"M4 1.5v15l12-7.5z"});
-    svg.append(pause,play);
-    return svg;
+    const icon=document.createElement("span");
+    icon.className="pause-white-icon";
+    icon.setAttribute("aria-hidden","true");
+    return icon;
   }
 
   function render(state){
