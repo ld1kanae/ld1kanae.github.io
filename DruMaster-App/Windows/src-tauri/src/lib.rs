b2ac74176297f3086ca5b5061dc2abfd15952386
ld1kanae <aoka45utau@gmail.com>
2026-09-01 00:25:43 +0900

    feat(DruMaster Windows): expose native close command

--- DruMaster-App/Windows/src-tauri/src/lib.rs
+++ DruMaster-App/Windows/src-tauri/src/lib.rs
@@ -1,6 +1,12 @@
+#[tauri::command]
+fn close_app(app: tauri::AppHandle) {
+    app.exit(0);
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![close_app])
         .run(tauri::generate_context!())
         .expect("error while running DruMaster");
 }
