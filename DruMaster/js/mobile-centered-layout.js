dbd1d33f1c6844069a27f6b13a0ae12ea20c4ffd
ld1kanae <aoka45utau@gmail.com>
2026-09-02 02:39:20 +0900

    Bust mobile result geometry cache

--- DruMaster/js/mobile-centered-layout.js
+++ DruMaster/js/mobile-centered-layout.js
@@ -16,7 +16,7 @@
   if(!document.querySelector('link[data-dm-result-mobile-centering]')){
     const link=document.createElement('link');
     link.rel='stylesheet';
-    link.href='css/result-mobile-centering-v2.css?v=20260902-v1';
+    link.href='css/result-mobile-centering-v2.css?v=20260902-score-nostroke1';
     link.dataset.dmResultMobileCentering='1';
     document.head.appendChild(link);
   }
