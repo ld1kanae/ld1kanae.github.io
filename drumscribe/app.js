02249731624b09b2ce0d378ebed49adf2e418cf6
ld1kanae <aoka45utau@gmail.com>
2026-09-23 16:04:03 +0900

    Bust eval cache for E-GMD v5

--- drumscribe/app.js
+++ drumscribe/app.js
@@ -1,4 +1,4 @@
-import {transcribe} from './transcribe.js?v=20260923-egmd-kst-v4';
+import {transcribe} from './transcribe.js?v=20260923-egmd-kst-v5';
 import {midiFile} from './midi.js?v=20260923-grid-v29';
 import {buildRhythmGrid,GRID_PPQ} from './rhythm-grid.js?v=20260923-grid-v29';
 import {inferBars,parseBeatThis} from './meter.js';
