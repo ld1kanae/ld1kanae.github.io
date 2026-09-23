926cbdc1f1fc045bd2ff4083123f164018ad179b
ld1kanae <aoka45utau@gmail.com>
2026-09-24 02:28:59 +0900

    drumscribe: bust transcribe cache after removing review hat repair

--- drumscribe/app.js
+++ drumscribe/app.js
@@ -1,4 +1,4 @@
-import {transcribe} from './transcribe.js?v=20260923-hat-sequence-v52';
+import {transcribe} from './transcribe.js?v=20260924-remove-review-hat-v67';
 import {analyzeSections,rescoreKstByArrangement,arrangementKstPolicyCurrent} from './arrangement/index.js?v=20260923-arrangement-kst-v47';
 import {midiFile} from './midi.js?v=20260923-tempo-bar-v35';
 import {buildRhythmGrid,GRID_PPQ} from './rhythm-grid.js?v=20260923-tempo-bar-v35';
